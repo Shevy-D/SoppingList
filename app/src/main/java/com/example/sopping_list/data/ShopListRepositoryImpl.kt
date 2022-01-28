3441827f63ac33bc591c9b5d5b5828fed7fb5654
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-01-28 20:44:37 +0500

    lesson 2

---
+++ app/src/main/java/com/example/sopping_list/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.example.sopping_list.data
+
+import com.example.sopping_list.domain.ShopItem
+import com.example.sopping_list.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(ShopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(ShopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(ShopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+}
