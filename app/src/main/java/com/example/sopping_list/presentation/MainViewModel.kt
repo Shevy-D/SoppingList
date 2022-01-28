90634371c9e559b82f298153241f3e0efeb198e5
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-01-28 22:25:26 +0500

    lesson 3

---
+++ app/src/main/java/com/example/sopping_list/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.sopping_list.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.sopping_list.data.ShopListRepositoryImpl
+import com.example.sopping_list.domain.DeleteShopItemUseCase
+import com.example.sopping_list.domain.EditShopItemUseCase
+import com.example.sopping_list.domain.GetShopListUseCase
+
+class MainViewModel : ViewModel {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+
+    }
+
+    fun deleteShopItemUseCase(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
